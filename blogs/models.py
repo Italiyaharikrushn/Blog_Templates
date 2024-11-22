e5e3d7daa3ad4f3198c6dad18eea8434abff0798
Italiyaharikrushn <italiyaharikrushn4@gmail.com>
2024-11-22 18:27:05 +0530

    done

--- blogs/models.py
+++ blogs/models.py
@@ -1,9 +1,20 @@
+import uuid
 from django.db import models
+
+def get_image_upload_to(instance, filename):
+    ext = filename.split('.')[-1]
+    filename = f"{uuid.uuid4()}.{ext}"
+    return f"profile_images/{filename}"
+
+def get_video_upload_to(instance, filename):
+    ext = filename.split('.')[-1]
+    filename = f"{uuid.uuid4()}.{ext}"
+    return f"profile_videos/{filename}"
 
 class Blog(models.Model):
     category = models.CharField(max_length=100)
     title = models.CharField(max_length=200)
-    image_url = models.ImageField(upload_to='profile_images/')
+    image_url = models.ImageField(upload_to=get_image_upload_to)
     post_title = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
@@ -13,28 +24,26 @@ class Blog(models.Model):
 class Travel(models.Model):
     title = models.CharField(max_length=200)
     description = models.CharField(max_length=500)
-    image_url = models.ImageField(upload_to='profile_images/')
-    # date = models.DateField()
+    image_url = models.ImageField(upload_to=get_image_upload_to)
     created_at = models.DateTimeField(auto_now_add=True)
     read_more_link = models.URLField(blank=True, null=True)
 
-
 class MySelf(models.Model):
-    video = models.FileField(upload_to='videos/', blank=True, null=True)
+    video = models.FileField(upload_to=get_video_upload_to, blank=True, null=True)
     title = models.CharField(max_length=200)
     description = models.CharField(max_length=500)
     created_at = models.DateTimeField(auto_now_add=True)
     read_more_link = models.URLField(blank=True, null=True)
 
 class PostPage(models.Model):
-    image_url = models.ImageField(upload_to='profile_images/')
+    image_url = models.ImageField(upload_to=get_image_upload_to)
     title = models.CharField(max_length=200)
     description = models.CharField(max_length=500)
     read_more_link = models.URLField(blank=True, null=True)
 
 class Biography(models.Model):
     name = models.CharField(max_length=255)
-    profile_img = models.ImageField(upload_to='profile_images/')
+    profile_img = models.ImageField(upload_to=get_image_upload_to)
     bio = models.TextField()
     facebook_link = models.URLField(blank=True, null=True)
     twitter_link = models.URLField(blank=True, null=True)
@@ -47,9 +56,9 @@ class Post(models.Model):
     description = models.TextField()
     date = models.DateField()
     status = models.CharField(max_length=50, blank=True, null=True)
-    
+
 class Advertisement(models.Model):
-    image = models.ImageField(upload_to='advertisements/')
+    image = models.ImageField(upload_to=get_image_upload_to)
     text = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
@@ -58,4 +67,3 @@ class Category(models.Model):
     description = models.TextField(blank=True, null=True)
     name = models.CharField(max_length=100, unique=True)
     item_count = models.PositiveIntegerField(default=0)
-    
--- blogs/urls.py
+++ blogs/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     # path('', views.create_entry, name='create_entry'),
@@ -14,5 +16,4 @@ urlpatterns = [
     path('posts/', views.post_view, name='posts'),
     path('advertisements/', views.advertisement_view, name='advertisements'),
     path('categories/', views.category_view, name='categories'),
-]
-
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

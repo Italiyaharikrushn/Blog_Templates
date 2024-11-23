e235037407638cfcdf86bb5de9250f57045963d4
Italiyaharikrushn <italiyaharikrushn4@gmail.com>
2024-11-23 16:24:12 +0530

    done

--- blogs/urls.py
+++ blogs/urls.py
@@ -4,10 +4,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    # path('', views.create_entry, name='create_entry'),
-    # path('entry-created/', views.entry_created, name='entry_created'),
-    # path('blog/', views.blog_view, name='blog'),
-
     path('', views.blog_view, name='blog'),
     path('travel/', views.travel_view, name='travel'),
     path('myself/', views.my_self_view, name='myself'),
@@ -17,6 +13,5 @@ urlpatterns = [
     path('advertisements/', views.advertisement_view, name='advertisements'),
     path('categories/', views.category_view, name='categories'),
 ]
-# + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

144cbf54c2d1e3d6e8437049bb29706a39d33005
Italiyaharikrushn <italiyaharikrushn4@gmail.com>
2024-11-23 14:38:33 +0530

    done

--- blogs/models.py
+++ blogs/models.py
@@ -15,7 +15,7 @@ def get_video_upload_to(instance, filename):
 class Blog(models.Model):
     category = models.CharField(max_length=100)
     title = models.CharField(max_length=200)
-    image_url = models.ImageField(upload_to='profile_images/')
+    image_url = models.ImageField(upload_to=get_image_upload_to)
     post_title = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
@@ -26,26 +26,27 @@ class Travel(models.Model):
     title = models.CharField(max_length=200)
     date = models.DateField(default=date.today)
     description = models.CharField(max_length=500)
-    image_url = models.ImageField(upload_to='profile_images/')
+    image_url = models.ImageField(upload_to=get_image_upload_to)
     created_at = models.DateTimeField(auto_now_add=True)
     read_more_link = models.URLField(blank=True, null=True)
 
 class MySelf(models.Model):
-    video = models.FileField(upload_to='profile_images/')
+    video = models.FileField(upload_to=get_video_upload_to)
+    # video = models.FileField(upload_to='profile_images/')
     title = models.CharField(max_length=200)
     description = models.CharField(max_length=500)
     created_at = models.DateTimeField(auto_now_add=True)
     read_more_link = models.URLField(blank=True, null=True)
 
 class PostPage(models.Model):
-    image_url = models.ImageField(upload_to='profile_images/')
+    image_url = models.ImageField(upload_to=get_image_upload_to)
     title = models.CharField(max_length=200)
     description = models.CharField(max_length=500)
     read_more_link = models.URLField(blank=True, null=True)
 
 class Biography(models.Model):
     name = models.CharField(max_length=255)
-    profile_img = models.ImageField(upload_to='profile_images/')
+    profile_img = models.ImageField(upload_to=get_image_upload_to)
     bio = models.TextField()
     facebook_link = models.URLField(blank=True, null=True)
     twitter_link = models.URLField(blank=True, null=True)
@@ -60,7 +61,7 @@ class Post(models.Model):
     status = models.CharField(max_length=50, blank=True, null=True)
 
 class Advertisement(models.Model):
-    image = models.ImageField(upload_to='profile_images/')
+    image = models.ImageField(upload_to=get_image_upload_to)
     text = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
